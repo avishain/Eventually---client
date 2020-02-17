07b13f5c1b093877099526fa6146f3a00e0c1f55
Avishai Nudelman <avishainu@gmail.com>
2020-02-17 22:12:01 +0200

    Update Top.js

--- src/components/shared/notificationsWindow/Top.js
+++ src/components/shared/notificationsWindow/Top.js
@@ -1,20 +1,25 @@
 import React from 'react';
-import { hidden } from 'ansi-styles';
 import logo from '../../../assets/images/calendar.png';
 
 const Top = (props) => {
     const numberOfNotifications = 2;
     return (
         <div style={containerStyle}>
-            <div>
-                <p style={{fontSize: 14}}>User notifications</p>
-                <p style={{fontSize: 12}}>{numberOfNotifications} new notifications</p>
+            <div style={leftPanelStyle}>
+                <p style={{...textStyle, fontWeight: 'bold', fontSize: 14, color: '#3A3B3F'}}>User notifications</p>
+                <p style={{...textStyle, fontSize: 12}}>{numberOfNotifications} new notifications</p>
             </div>
             <div style={rightPanelStyle}>
                 <img src={logo} alt='Eventually!' style={imageStyle} />
             </div>
         </div>
     );
+}
+
+const textStyle = {
+    fontFamile: 'Roboto',
+    color: '#9EA0A5',
+    margin: 0
 }
 
 const containerStyle = { 
@@ -28,7 +33,14 @@ const containerStyle = {
 }
 
 const rightPanelStyle = {
+    width: '60%',
     background: 'radial-gradient(50% 50% at 50% 50%, rgba(103, 54, 107, 0.3) 44.79%, rgba(196, 196, 196, 0) 100%)'
+}
+
+const leftPanelStyle = {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'center'
 }
 
 const imageStyle = {
